84dd259cbdc5e8e3b11c1e79677ac27ccc123fc2
acap910 <2023437202@student.uitm.edu.my>
2025-01-14 11:14:15 +0800

    Post new chats to Firestore

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,14 +11,7 @@ import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(environment.firebase)),
-      provideFirestore(() => getFirestore()),
-      provideAuth(() => getAuth()),
-      provideFunctions(() => getFunctions()),
-      provideStorage(() => getStorage()),
-      provideMessaging(() => getMessaging())
-    ),
+    
     provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-c6149","appId":"1:882520665603:web:03760ad6a215d7a729a836","storageBucket":"friendlychat-c6149.firebasestorage.app","apiKey":"AIzaSyCumLvy6Na0wR3Z1GgcxoWAJFBCTGQVzHM","authDomain":"friendlychat-c6149.firebaseapp.com","messagingSenderId":"882520665603","measurementId":"G-9HZRE67Y26"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
   ],
 };
